8dc21541f045dc675a9f40f4a915de660287d86c
jeremyfelt <jeremy.felt@gmail.com>
2015-05-11 14:59:31 -0700

    Setup initial plugin files

---
+++ wsuwp-html-snippets.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: WSU HTML Snippets
+Version: 0.0.1
+Description: Embed common HTML content throughout a WordPress site.
+Author: washingtonstateuniversity, jeremyfelt
+Author URI: https://web.wsu.edu/
+Plugin URI: https://web.wsu.edu/
+Text Domain: wsuwp-html-snippets
+Domain Path: /languages
+*/
+
+class WSU_HTML_Snippets {
+	public function __construct() {
+	}
+}
+new WSU_HTML_Snippets();

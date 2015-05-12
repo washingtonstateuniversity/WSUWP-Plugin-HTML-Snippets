6497d88ba54535d75b65f07b551e95671ad3bbea
jeremyfelt <jeremy.felt@gmail.com>
2015-05-12 09:44:17 -0700

    Setup initial HTML Snippets shortcode with tests

--- wsuwp-html-snippets.php
+++ wsuwp-html-snippets.php
@@ -11,7 +11,74 @@ Domain Path: /languages
 */
 
 class WSU_HTML_Snippets {
+	/**
+	 * @var string The slug used for registering the content type.
+	 */
+	static $content_type_slug = 'wsu_html_snippet';
+
+	/**
+	 * Setup the plugin.
+	 */
 	public function __construct() {
+		add_action( 'init', array( $this, 'register_content_type' ) );
+		add_shortcode( 'html_snippet', array( $this, 'display_html_snippet' ) );
+	}
+
+	/**
+	 * Register the content type to be used for HTML Snippets
+	 */
+	public function register_content_type() {
+		$labels = array(
+			'name' => 'HTML Snippets',
+			'singular_name' => 'HTML Snippet',
+			'add_new' => 'Add New',
+			'add_new_item' => 'Add New HTML Snippet',
+			'edit_item' => 'Edit HTML Snippet',
+			'new_item' => 'New HTML Snippet',
+			'all_items' => 'All HTML Snippets',
+			'view_item' => 'View HTML Snippets',
+			'search_items' => 'Search HTML Snippets',
+			'not_found' => 'No HTML snippets found',
+			'not_found_in_trash' => 'No HTML snippets found in Trash',
+			'menu_name' => 'HTML Snippets',
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => false,
+			'publicly_queryable' => false,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => false,
+			'has_archive'        => false,
+			'hierarchical'       => false,
+			'supports'           => array( 'title', 'editor' ),
+		);
+		register_post_type( $this::$content_type_slug, $args );
+	}
+
+	public function display_html_snippet( $atts ) {
+		$default_atts = array(
+			'id' => 0,
+		);
+		$atts = wp_parse_args( $atts, $default_atts );
+
+		if ( empty( $atts['id'] ) ) {
+			return '';
+		}
+
+		if ( 0 === absint( $atts['id'] ) ) {
+			return '';
+		}
+
+		$post = get_post( $atts['id'] );
+
+		if ( ! $post || $this::$content_type_slug !== $post->post_type ) {
+			return '';
+		}
+
+		return apply_filters( 'the_content', $post->post_content );
 	}
 }
 new WSU_HTML_Snippets();

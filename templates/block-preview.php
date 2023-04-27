ac75757761d898aa43fb61987e0b6e9aab7544db
Dan White <dan.white1@wsu.edu>
2023-04-27 16:18:05 -0700

    Fix | Some components not previewing

--- templates/block-preview.php
+++ templates/block-preview.php
@@ -2,9 +2,19 @@
 <style>
     html{
         margin-top: 0 !important;
+    }    
+    .wsu-wrapper-content{
+        padding: 0 !important;
+    }
+    .wsu-wrapper-content::before{
+        background-color: transparent !important;
     }
 </style>
-<div id="wsu-gutenberg-snippet-preview">
-    <?php the_content(); ?>
+<div class="wsu-wrapper-content ">
+    <main role="main" id="wsu-content" class="wsu-wrapper-main" tabindex="-1">
+        <div id="wsu-gutenberg-snippet-preview">
+            <?php the_content(); ?>
+        </div>
+    </main>
 </div>
 <?php get_footer(); ?>

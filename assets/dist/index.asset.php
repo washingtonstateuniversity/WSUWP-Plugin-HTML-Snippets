85dc98eeff0afc857db688592065bc48e9ee8bac
Dan White <dan.white1@wsu.edu>
2023-10-31 15:08:41 -0700

    build assets

--- assets/dist/index.asset.php
+++ assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '552456ab5d783a21e14a');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => 'd06f31f94fe8ec4b6257');
